aa813e8968915e219c858447e77d0a323a08410b
AjayYadavAi <proajayyadav0525@gmail.com>
2020-07-01 18:28:05 +0530

    Adding theme

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <div class="wrapper" id="home">
+      <Nav></Nav>
+      <Banner></Banner>
+      <LatestAlbum></LatestAlbum>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue'
+import Banner from './components/Banner.vue'
+import LatestAlbum from './components/LatestAlbum.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Nav,
+    Banner,
+    LatestAlbum
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
