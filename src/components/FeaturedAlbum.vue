3cb8666295a2e51f39aa14dcef68edb57b5c159a
AjayYadavAi <proajayyadav0525@gmail.com>
2020-07-08 20:30:50 +0530

    Change

--- src/components/FeaturedAlbum.vue
+++ src/components/FeaturedAlbum.vue
@@ -11,13 +11,13 @@
 			<div class="featured-element">
 				<div class="row">
 					<div class="col-md-4 col-sm-6" v-for="album in musiclist" :key="album.id">
+		
 						<!-- featured item -->
 						<div class="featured-item ">
 							<!-- image container -->
-							<div class="figure" v-if="albumImage(album.id)">
+							<div class="figure">
 								<!-- image -->
-								{{ albumImage(album.id) }}
-								<img class="img-responsive" :src="albumImage(album.id)" alt="" />
+								<img class="img-responsive" :src="album.id" alt="" />
 								<!-- paragraph -->
 								<p>There are many variations of passages available, but the majority have suffered Lorem alteration in some form, by injected look even slightly believable.</p>
 							</div>
@@ -33,6 +33,7 @@
 						</div>
 					</div>
 				</div>
+										{{ images }}
 			</div>
 		</div>
 	</div>
@@ -45,7 +46,8 @@
 		data(){
 			return {
 				publicPath: process.env.BASE_URL,
-				musiclist:{}
+				musiclist:{},
+				images:[]
 			}
 		},
 		mounted() {
@@ -57,14 +59,13 @@
 				const res = await fetch(api);
 				const data = await res.json();
 				this.musiclist = data.albums;
+				for(var i=0;i<this.musiclist.length;i++){
+					var apiimg = "https://api.napster.com/v2.1/albums/"+this.musiclist[i].id+"/images?apikey="+process.env.VUE_APP_API_KEY;
+					axios.get(apiimg).then((response)=>{
+						this.images[i] = response.data.images[2].url;
+					});
+				}
 			},
-			albumImage(id){
-				var api = "https://api.napster.com/v2.1/albums/"+id+"/images?apikey="+process.env.VUE_APP_API_KEY;
-				axios.get(api).then((response)=>{
-					var path = response.data.images[2].url;
-					return path;
-				});
-			}
-		}
+		},
 	}
 </script>
